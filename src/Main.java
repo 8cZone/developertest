15b7891454d5e7f6e5c26b2dede67adef93d6f64
8czone <619813006@qq.com>
2019-10-31 20:03:00 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.StringUtils;
+public class Main {
+    public static void main(String[] args) {
+        PasswordIdentify.identify("1a2b");
+        PasswordIdentify.identify("1a2b**");
+    }
+}
